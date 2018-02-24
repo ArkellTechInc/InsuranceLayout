373c51aa1a7025f93f56373a97b2749bd53d9f0e
root <root@DESKTOP-V0ECS0I.localdomain>
2018-02-24 10:46:19 -0500

    stuff

--- html/scripts/dopamine.js
+++ html/scripts/dopamine.js
@@ -1,5 +1,5 @@
 //Id names of elements that will be affected by orientation changes
-var elements = ["topMenu", "menuButton", "container", "botMenu", "sideMenu", "content"];
+var elements = ["topMenu", "menuButton", "container", "botMenu", "sideMenu", "content", "topTitle"];
 
 var menuVisible;
 
