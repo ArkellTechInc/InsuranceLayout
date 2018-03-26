160d1f0fa2970caec3461516884dbea9f21de8da
root <root@DESKTOP-V0ECS0I.localdomain>
2018-03-26 16:45:56 -0400

    made a thing

--- html/scripts/dopamine.js
+++ html/scripts/dopamine.js
@@ -89,6 +89,7 @@ function switchOrientation(mode) {
 			hideMenu();
 			
 			window.scrollTo(0, 0); //scrolls to top
+			// window.scrollTo(0,$(document).height()-$(window).height()); //scrolls to bottom
 			$(".swiperFix1").addClass("swiper-container");
 			$(".swiperFix2").addClass("swiper-wrapper");
 			//Enable pagination
@@ -97,7 +98,7 @@ function switchOrientation(mode) {
 			}
 			
 			//Prevent vertical scrolling
-			$("body").css("overflow-y", "hidden");
+			//$("body").css("overflow-y", "hidden");
 			
 			togglePaginationClasses(mode);
 			
